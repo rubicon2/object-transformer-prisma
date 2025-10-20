4580b0e9c2cee5417bf62d629295de64f0af1cb3
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-10-20 16:02:48 +0100

    Before each integration test, clear database and populate with test data

--- tests/setup.mjs
+++ tests/setup.mjs
@@ -1,5 +1,70 @@
+import { PrismaClient } from '../generated/prisma/client';
 import { beforeEach } from 'vitest';
 
-beforeEach(() => {
-  // Clear database, populate with fresh data.
+const db = new PrismaClient();
+
+export const users = [
+  {
+    id: 'abc',
+    createdAt: new Date(Date.now()),
+    email: 'jimbo.johnson@outlook.com',
+    first_name: 'Jimbo',
+    last_name: 'Johnson',
+  },
+  {
+    id: 'def',
+    createdAt: new Date(Date.now()),
+    email: 'flambo.blobson@outlook.com',
+    first_name: 'Flambo',
+    last_name: 'Blobson',
+  },
+  {
+    id: 'ghi',
+    createdAt: new Date(Date.now()),
+    email: 'nadine.blobbers@outlook.com',
+    first_name: 'Nadine',
+    last_name: 'Blobbers',
+  },
+  {
+    id: 'jkl',
+    createdAt: new Date(Date.now()),
+    email: 'jennifer.blobbers@outlook.com',
+    first_name: 'Jennifer',
+    last_name: 'Blobbers',
+  },
+];
+
+export const files = [
+  {
+    id: '123',
+    createdAt: new Date(Date.now()),
+    ownerId: 'abc',
+    url: 'my-first-file.txt',
+  },
+  {
+    id: '456',
+    createdAt: new Date(Date.now()),
+    ownerId: 'abc',
+    url: 'my-second-file.txt',
+  },
+  {
+    id: '789',
+    createdAt: new Date(Date.now()),
+    ownerId: 'def',
+    url: 'my-third-file.txt',
+  },
+];
+
+beforeEach(async () => {
+  // Clear database, populate with test data.
+  await db.$transaction([
+    db.user.deleteMany(),
+    db.file.deleteMany(),
+    db.user.createMany({
+      data: users,
+    }),
+    db.file.createMany({
+      data: files,
+    }),
+  ]);
 });
